1c1a2deb74d76f817c95b71541e3f8c341c68e41
thejabban <109586219+thejabban@users.noreply.github.com>
2022-08-03 22:28:01 +0300

    done

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textview =findViewById(R.id.calculator);
+        TextView textView1=findViewById(R.id.total);
+        EditText edittext=findViewById(R.id.name);
+        EditText edittext1=findViewById(R.id.second);
+        Button button= findViewById(R.id.button);
     }
 }
